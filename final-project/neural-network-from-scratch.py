0eff137deb7337237d77f877ed956e53edd8b7c9
Quang <quangcrazymen@gmail.com>
2022-11-21 09:47:41 +0700

    New docs

---
+++ final-project/neural-network-from-scratch.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+# forward propagation
+
+# backward propagation
+
---
+++ numpy-test/test.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+a = np.array([[1,2,3],[12,34,52]])
+print(np.trace(a))
